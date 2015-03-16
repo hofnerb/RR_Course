33ff4860a96af267ca472f47ddd611a6a736cf65
Benjamin Hofner <benjamin.hofner@fau.de>
2015-03-16 19:50:43 +0100

    updated R source file which results from purl("first_knitr.Rnw")

--- first_knitr.R
+++ first_knitr.R
@@ -1,40 +1,56 @@
 ## ----load_packages, include=FALSE----------------------------------------
 library("xtable")
 
-## ----test name, echo=TRUE, results='markup'------------------------------
+## ----print_x, echo=TRUE, results='markup'--------------------------------
 x <- 1:10
-plot(x)
 print(x)
 
-## ----echo=TRUE, results='asis', fig.width=5, fig.height = 5, out.width="0.4\\textwidth"----
+## ----print_x_as_is, echo=TRUE, results='asis'----------------------------
 x <- 1:10
-plot(x)
 print(x)
+
+## ----plot_x--------------------------------------------------------------
+plot(x)
+
+## ----two_figures, fig.width=5, fig.height = 5, out.width="0.4\\textwidth", fig.show='hold'----
+plot(x)
 plot(rnorm(100))
 
-## ----init, include=FALSE-------------------------------------------------
-## from now on no echo
-opts_chunk$set(echo = FALSE)
+## ----label with spaces, out.width="0.4\\textwidth"-----------------------
+plot(1:5)
 
-## ----create_table, results='asis'----------------------------------------
+## ----init----------------------------------------------------------------
+## from now on no syntax higlighting
+opts_chunk$set(highlight = FALSE)
+
+## ----create_table--------------------------------------------------------
 TAB <- table(x)
+TAB
+
+## ----pretty_table, results='asis'----------------------------------------
 xtable(TAB)
 
 ## ----first_cache, cache=TRUE---------------------------------------------
-Sys.sleep(2)
+## use Sys.sleep to see that it is only executed in the first run
+Sys.sleep(5)
 plot(1:3, main = "cached figure", xlab = "x")
 
 ## ----random, cache=TRUE--------------------------------------------------
 set.seed(1234)
-rnorm(100)
+y <- rnorm(100)
+mean(y)
 
 ## ------------------------------------------------------------------------
 rnorm(1)
 
-## ----cache = TRUE, random_seed = rand_seed-------------------------------
+## ----cache = TRUE, cache.seed = rand_seed--------------------------------
 rnorm(10)
 
+## ----random_seed---------------------------------------------------------
+opts_chunk$set(cache.seed = rand_seed)
+
 ## ----eval = FALSE--------------------------------------------------------
-## ## extract code
+## library("knitr")
+## ## extract code.
 ## purl("first_knitr.Rnw", documentation = 1)
 
